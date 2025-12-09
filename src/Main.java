09baa0a46c7c2a67011bb42f8b3248676a123c14
Manantena17 <raheriniaina.man@zurcher.edu.mg>
2025-12-09 15:43:25 +0300

    git remote add origin https://github.com/Manantena17/javaProjetMisemestre.git git branch -M main git push -u origin main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main {
     private static double[][] notes = new double[4][5];
 //    private static int nombreEtudiantsSaisis = 0;
+    private static String [] matieres={"Java Oriente Objet","C#","PHP","Teleinformatique","Python" };
     public static void main(String[] args) {
 
         menu();
@@ -9,8 +10,7 @@ public class Main {
     public static void menu() {
         Scanner input = new Scanner(System.in);
         int choix = -1;
-
-        while (choix != 5) { // Boucle tant que l'utilisateur ne choisit pas "Quitter"
+        while (choix != 5) {
             System.out.println("\n--- MENU ---");
             System.out.println("1 - Saisie des notes de 4 étudiants");
             System.out.println("2 - Afficher les notes et la moyenne des étudiants");
@@ -47,15 +47,17 @@ public class Main {
         }
     }
 
-    /**
-     * Permet de saisir les notes pour un nombre fixe d'étudiants (4) et de notes (5).
-     */
     public static void saisirNotesEtudiants() {
         int numeroEtudiant;
+        int indexCours;
+
         Scanner input = new Scanner(System.in);
         System.out.println("\n--Entrez les numero des étudiants commencer par le 0 ---");
         numeroEtudiant=input.nextInt();
-
+        System.out.println("Entrez le numero de matiere commencer par 0");
+        indexCours=input.nextInt();
+        System.out.println("Entrez le  note de "+matieres[indexCours]+":");
+        notes[numeroEtudiant][indexCours]= input.nextDouble();
 //        int NB_ETUDIANTS = notes.length; // 4
 //        int NB_NOTES = notes[0].length; // 5
 
@@ -65,16 +67,15 @@ public class Main {
 //            notes[i][i]=input.nextDouble();
 //             On pourrait demander le nom de l'étudiant ici, mais on se contente des notes.
 
-            for (int j =0; j<5; j++) {
-                System.out.print("Entrez la note " + (j + 1) + " (entre 0 et 20) : ");
-                notes[numeroEtudiant][j]=input.nextDouble();
-              //  double noteSaisie = input.nextDouble();
-                // Stockage de la note dans le tableau global statique 'notes'
-               // notes[i][j] = noteSaisie;
-            }
+//            for (int j =0; j<5; j++) {
+//                System.out.print("Entrez la note " + (j + 1) + " (entre 0 et 20) : ");
+//                notes[numeroEtudiant][j]=input.nextDouble();
+//              //  double noteSaisie = input.nextDouble();
+//                // Stockage de la note dans le tableau global statique 'notes'
+//               // notes[i][j] = noteSaisie;
+//            }
 
 //        nombreEtudiantsSaisis = 4;
-        System.out.println("\n Saisie terminée. Les notes de 4 étudiants ont été enregistrées.");
     }
 
     public static void afficherResultats() {
@@ -83,8 +84,6 @@ public class Main {
 //            System.out.println(" Aucune note n'a été saisie. Veuillez utiliser l'option 1 d'abord.");
 //            return;
 //        }
-
-        System.out.println("\n--- Résultats des étudiants ---");
         System.out.println("Entrez le numero des etudiants que vous regardez");
 
         int voirNoteEtudiant;
@@ -92,10 +91,11 @@ public class Main {
 //        for (int i = 1; i < 5; i++) {
 //            double sommeNotes = 0.0;
 //            System.out.print("Étudiant " + (i + 1) + " | Notes : [");
+        System.out.println("Le note de l'etudieant de numero "+voirNoteEtudiant+"sont");
             for (int j = 0; j < 5; j++) {
                 double note = notes[voirNoteEtudiant][j];
-                System.out.print("Le note de l'etudieant de numero "+voirNoteEtudiant+"=");
-                System.out.println(note + (j < 5 - 1 ? ", " : "")); // Ajoute une virgule sauf pour la dernière note
+                System.out.println(matieres[j]+"="+ notes[voirNoteEtudiant][j]);
+                //System.out.println(note + (j < 5 - 1 ? ", " : "")); // Ajoute une virgule sauf pour la dernière note
 //                sommeNotes += note;
             }
 
@@ -108,26 +108,34 @@ public class Main {
     public static void modifierNotes(){
         Scanner input = new Scanner(System.in);
         int modifyNote;
-
+        int numeroEtudiant;
         System.out.println("Entrez le numero de l'etudiant modifier de notes");
+        numeroEtudiant=input.nextInt();
+        System.out.println("Entrez le id de matiere ");
         modifyNote=input.nextInt();
         double note;
-        for (int i=0;i<5;i++){
-            note=input.nextDouble();
-            notes[modifyNote][i]=note;
-
-        }
+        System.out.println("Entrez le nouveaux note de "+matieres[modifyNote]);
+        note=input.nextDouble();
+        notes[numeroEtudiant][modifyNote]=note;
+//        for (int i=0;i<5;i++){
+//
+//            note=input.nextDouble();
+//            notes[modifyNote][i]=note;
+//
+//        }
 
     }
     public static void effacernote(){
         Scanner input = new Scanner(System.in);
         int deleteNote;
+        int noteEffacer;
         System.out.print("Entrez le numero de l'etudiant à effacer de note :");
         deleteNote=input.nextInt();
+        System.out.println("entrez le numero de cours effacer de note");
+        noteEffacer= input.nextInt();
         double note;
-        for (int i=0;i<5;i++){
-            note =0;
-            notes[deleteNote][i]=note;
-        }
+        note =0;
+        notes[deleteNote][noteEffacer]=note;
+        System.out.println("Suppression reussie ");
     }
 }
