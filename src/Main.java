396975ac9003b4a84f2623d228125bafdf698fc7
Manantena17 <raheriniaina.man@zurcher.edu.mg>
2025-12-10 08:53:37 +0300

    git remote add origin https://github.com/Manantena17/javaProjetMisemestre.git git branch -M

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class Main {
     private static double[][] notes = new double[4][5];
-//    private static int nombreEtudiantsSaisis = 0;
-    private static String [] matieres={"Java Oriente Objet","C#","PHP",
-        "Teleinformatique","Python" };
-    private static String [] nom = new String[4];
+    private static String[] matieres = {"Java Oriente Objet", "C#", "PHP",
+            "Teleinformatique", "Python"};
+    private static String[] nom = new String[4];
+
     public static void main(String[] args) {
 
         menu();
@@ -18,7 +18,6 @@ public class Main {
             System.out.println("2 - Afficher les notes et la moyenne des étudiants");
             System.out.println("3 - Modifier les notes");
             System.out.println("4 - Effacer les notes");
-
             System.out.println("5 - Quitter");
             System.out.print("Entrez votre choix : ");
 
@@ -33,8 +32,10 @@ public class Main {
                         break;
                     case 3:
                         modifierNotes();
+                        break;
                     case 4:
                         effacernote();
+                        break;
                     case 5:
                         System.out.println("Au revoir !");
                         break;
@@ -54,57 +55,76 @@ public class Main {
         int indexCours;
 
         Scanner input = new Scanner(System.in);
-        System.out.println("\n--Entrez les numero des étudiants entre par le 0 et 3 ---");
-        numeroEtudiant=input.nextInt();
-        System.out.println("Entrez le nom etudiant");
-        input.nextLine();
-        nom[numeroEtudiant]=input.nextLine();
-        System.out.println("Entrez le numero de matiere entre 0 et 4");
-        indexCours=input.nextInt();
-        System.out.println("Entrez le  note de "+matieres[indexCours]+":");
-        if(notes[numeroEtudiant][indexCours]==0){
-            notes[numeroEtudiant][indexCours]= input.nextDouble();
-        }
-        else {
-            System.out.println("Erreur le note de matiere " + matieres[indexCours] + " " +
-                    "deja rempli donc vous allez au modifier note");
+        System.out.println("\n--Entrez les numero des étudiants entre par le 1 et 4 ---");
+        numeroEtudiant = input.nextInt();
+        if (nom[numeroEtudiant] == null || notes[numeroEtudiant][0] == 0
+                || notes[numeroEtudiant][1] == 0
+                || notes[numeroEtudiant][2] == 0
+                || notes[numeroEtudiant][3] == 0
+                || notes[numeroEtudiant][4] == 0) {
+            if (nom[numeroEtudiant] == null) {
+                System.out.println("Entrez le nom etudiant");
+                input.nextLine();
+                nom[numeroEtudiant] = input.nextLine();
+            }
+            System.out.println("Entrez le numero de matiere entre 1 et 5");
+            indexCours = input.nextInt();
+            System.out.println("Entrez le  note de " + nom[numeroEtudiant] + "  "
+                    + matieres[indexCours] + ":");
+            if (notes[numeroEtudiant][indexCours] == 0) {
+                notes[numeroEtudiant][indexCours] = input.nextDouble();
+            } else {
+                System.out.println("Erreur le note de matiere " + matieres[indexCours] + " " +
+                        "deja rempli donc vous allez au modifier note");
+            }
+        } else {
+            System.out.println("le notes de " + nom[numeroEtudiant] + " deja remmplir ");
         }
     }
+
     public static void afficherResultats() {
         Scanner input = new Scanner(System.in);
         System.out.println("Entrez le numero des etudiants que vous regardez");
         int voirNoteEtudiant;
-        voirNoteEtudiant=input.nextInt();
-        System.out.println("Le note de " +nom[voirNoteEtudiant]+ " sont");
-        System.out.println(nom[0]);
-            for (int j = 0; j < 5; j++) {
-                System.out.println(matieres[j]+"="+ notes[voirNoteEtudiant][j]);
-            }
+        voirNoteEtudiant = input.nextInt();
+        System.out.println("Le note de " + nom[voirNoteEtudiant] + " sont");
+        for (int j = 0; j < 5; j++) {
+            System.out.println(matieres[j] + "=" + notes[voirNoteEtudiant][j]);
+        }
     }
-    public static void modifierNotes(){
+
+    public static void modifierNotes() {
         Scanner input = new Scanner(System.in);
         int modifyNote;
         int numeroEtudiant;
         System.out.println("Entrez le numero de l'etudiant modifier de notes");
-        numeroEtudiant=input.nextInt();
+        numeroEtudiant = input.nextInt();
         System.out.println("Entrez le id de matiere ");
-        modifyNote=input.nextInt();
+        modifyNote = input.nextInt();
         double note;
-        System.out.println("Entrez le nouveaux note de "+matieres[modifyNote]);
-        note=input.nextDouble();
-        notes[numeroEtudiant][modifyNote]=note;
+        if(nom[numeroEtudiant]!=null&&notes[numeroEtudiant][modifyNote]!=0){
+            System.out.println("Entrez le nouveaux note de " + nom[numeroEtudiant] +
+                    " " + matieres[modifyNote]);
+            note = input.nextDouble();
+            notes[numeroEtudiant][modifyNote] = note;
+        }else {
+            System.out.println("Aucune etudiant correspondent a la numero");
+        }
+
     }
+
     public static void effacernote(){
         Scanner input = new Scanner(System.in);
         int deleteNote;
         int noteEffacer;
         System.out.print("Entrez le numero de l'etudiant à effacer de note :");
         deleteNote=input.nextInt();
-        System.out.println("entrez le numero de cours effacer de note");
+        System.out.println("entrez le numero de cours effacer ");
         noteEffacer= input.nextInt();
         double note;
         note =0;
         notes[deleteNote][noteEffacer]=note;
-        System.out.println("Suppression reussie ");
+        System.out.println("Suppression reussie le note de "+nom[deleteNote] +" "+
+                matieres[noteEffacer]);
     }
 }
